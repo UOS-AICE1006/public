0e5dfef6bad38919b558e7cf7cfd1f486e7d7d48
Hikmat Farhat <h.farhat@soton.ac.uk>
2025-02-20 15:58:37 +0000

    update

--- xml/xpath-examples.sh
+++ xml/xpath-examples.sh
@@ -9,3 +9,4 @@ xmllint --xpath '//project[supervisor/name[contains(text(),"Stuart")]]/keywords'
 xmllint --xpath '//project[keywords/keyword[contains(text(),"Data")]]/supervisor/name/text()' projects.xml 
 xmllint --xpath '//project[*/keyword[contains(text(),"Data")]]/supervisor/name/text()' projects.xml
 xmllint --xpath '//project[*/keyword[contains(text(),"Data") or contains(text(),"Nano")]]/supervisor/name/text()' projects.xml
+xmllint --xpath '/projects/project/supervisor[contains(../keywords/keyword/text(),"Swarm")]' projects.xml
